320304ddc85290db1ff7b6e94705f78ea3dca643
kumar ankesh <kumarankesh1894@gmail.com>
2025-07-02 13:05:13 +0530

    Update API integration in frontend again

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:8080/api/" || "https://mari-ai-backend.onrender.com",
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:8080/api",
 });
 
-export const GetPosts = async () => await API.get("/post/");
-export const CreatePost = async (data) => await API.post("/post/", data);
+export const GetPosts = async () => await API.get("/post");
+export const CreatePost = async (data) => await API.post("/post", data);
 export const GenerateAIImage = async (data) =>
-  await API.post("/generateImage/", data);
+  await API.post("/generateImage", data);
