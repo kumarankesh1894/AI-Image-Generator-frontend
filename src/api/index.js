990a377ed5598e711b4997ee2d67c93e78ff54f5
kumar ankesh <kumarankesh1894@gmail.com>
2025-07-02 13:08:58 +0530

    Update API integration in frontend again

--- src/api/index.js
+++ src/api/index.js
@@ -7,4 +7,4 @@ const API = axios.create({
 export const GetPosts = async () => await API.get("/post");
 export const CreatePost = async (data) => await API.post("/post", data);
 export const GenerateAIImage = async (data) =>
-  await API.post("/generateImage", data);
+  await API.post("/api/generateImage", data);
