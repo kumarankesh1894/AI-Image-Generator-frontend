bed5d5fe55c3f56529caa6dfadbb0f9bd976739f
kumar ankesh <kumarankesh1894@gmail.com>
2025-07-02 12:35:24 +0530

    Initial commit: React frontend code

---
+++ src/api/index.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:8080/api/",
+});
+
+export const GetPosts = async () => await API.get("/post/");
+export const CreatePost = async (data) => await API.post("/post/", data);
+export const GenerateAIImage = async (data) =>
+  await API.post("/generateImage/", data);
